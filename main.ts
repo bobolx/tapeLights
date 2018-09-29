fe868a7cca356b52f81c4c56a03623ecaabdc4a7
lixiang <214372150@qq.com>
2018-09-29 09:57:17 +0800

    update max led num

--- main.ts
+++ main.ts
@@ -3,14 +3,14 @@
 //% weight=99 icon="\uf0e7" color=#1B80C4
 namespace tapeLights {
 
-    let maxRGB = 24;
+    let maxRGB = 60;
     let neoStrip = neopixel.create(DigitalPin.P1, maxRGB, NeoPixelMode.RGB);
 
     //% blockId=tape_rgb block="灯%firstRGB| ~ 灯%lastRGB| 亮，颜色为 R值 %red| G值 %green| B值 %blue"
-    //% firstRGB.min=1 firstRGB.max=24
+    //% firstRGB.min=1 firstRGB.max=60
     //% firstRGB.defl=1
-    //% lastRGB.min=1 lastRGB.max=24
-    //% lastRGB.defl=24
+    //% lastRGB.min=1 lastRGB.max=60
+    //% lastRGB.defl=60
     //% red.min=0 red.max=255
     //% green.min=0 green.max=255
     //% blue.min=0 blue.max=255
@@ -29,7 +29,6 @@ namespace tapeLights {
             neoStrip.setPixelColor(i, neopixel.rgb(red, green, blue));
         }
         
-
         neoStrip.show();
         
     }
@@ -37,12 +36,10 @@ namespace tapeLights {
    /**
      * 关闭所有灯
      */
-   //% blockId="cruise_neo_clear" block="关闭所有灯"
-   //% weight=55
+   //% blockId="tape_neo_clear" block="关闭所有灯"
+   //% weight=98
    export function neoClear(): void {
        neoStrip.showColor(neopixel.colors(NeoPixelColors.Black));
    }
 
 }
-
-
